6112cf2f448ef5144a086a39cdbeefbc22452ddf
Erika Tobias <et5392@rit.edu>
2017-11-21 15:52:40 -0500

    conflicts resolved
    fixed two file conflicts which were merged.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PhotoListComponent } from './photos/photo-list.component';
-<<<<<<< HEAD
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-=======
+
 import { PhotoComponent } from './photo/photo.component';
->>>>>>> 801e7cc1f1f182ffd21fcc9f87c1a366efbf0dfa
+
 
 @NgModule({
   declarations: [
@@ -17,12 +16,8 @@ import { PhotoComponent } from './photo/photo.component';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
 	FormsModule,
 	AngularFontAwesomeModule
-=======
-	  FormsModule
->>>>>>> 801e7cc1f1f182ffd21fcc9f87c1a366efbf0dfa
   ],
   providers: [],
   bootstrap: [AppComponent]
