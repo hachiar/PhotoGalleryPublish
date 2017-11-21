801e7cc1f1f182ffd21fcc9f87c1a366efbf0dfa
Kristen Merritt <km2029@rit.edu>
2017-11-21 15:37:02 -0500

    added single photo stuff to the same page, unsure of how to split it into a different page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {PhotoListComponent} from './photos/photo-list.component';
+import { PhotoListComponent } from './photos/photo-list.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-	PhotoListComponent
+	  PhotoListComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
-	FormsModule
+	  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/photo/photo.ts
@@ -0,0 +1,8 @@
+export interface IPhoto {
+	id:number;
+	name:string;
+	url:string;
+	rating:number;
+	ratingArray:Array;
+	favorite:boolean;
+}
