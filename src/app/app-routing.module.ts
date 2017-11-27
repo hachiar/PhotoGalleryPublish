7694ac599677c6d8ba12d0dcf8e717e22c1675d4
David Camacho <administrator@Ground-Mac.rochester.rr.com>
2017-11-27 17:11:08 -0500

    added routing file and updated app.module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photos/photos.component';
+import { HeartComponent } from './shared/heart.component';
+import { StarComponent } from './shared/star.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+const routes: Routes = [
+  { path: 'photo', component: PhotoComponent },
+  { path: 'photo-list', component: PhotoListComponent },
+  { path: 'heart', component: HeartComponent },
+  { path: 'star', component: StarComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: '',
+    redirectTo: '/photo',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
