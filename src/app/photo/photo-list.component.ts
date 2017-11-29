6c1dd1ac054d2d4a9af9ece129652852df6dbfb4
Erika Tobias <et5392@rit.edu>
2017-11-29 11:38:08 -0500

    Some Compilation issues fixed
    Fixed some issues with the compiler

--- src/app/photo/photo-list.component.ts
+++ src/app/photo/photo-list.component.ts
@@ -26,25 +26,25 @@ export class PhotoListComponent implements OnInit{
 		console.log("Wir haben die Wertung " + rate + " erhalten.");
 	}//end function: updateRating
 	
-	updateFav(fav:boolean, photo:Photo): void{
-		console.log("Dieses Bild ist dein Lieblingsbild: " + fav.toString());
-		//photo.switchFavorite();
-		this.favoritePhotos = this.setFavorites();
-	}//end function: updateFav
-	
 	ngOnInit():void{
 		console.log("On Init - PhotoListComponent");
 		this.photos = this._photoService.getPhotos();
 		this.favoritePhotos = this.setFavorites();
 	}//end function: ngOnInit
 	
+	
+	updateFav(fav:boolean, photo:Photo): void{
+		console.log("Dieses Bild ist dein Lieblingsbild: " + fav.toString());
+		//photo.switchFavorite();
+		this.favoritePhotos = this.setFavorites();
+	}//end function: updateFav
+	
+	
+	
 	setFavorites():Photo[]{
 		return this.photos.filter((photo: Photo) => 
 		photo.favorite == true);
 	}//end function: setFavorites
-	
-	
-	
 	
 }
 
--- src/app/photo/photo.ts
+++ src/app/photo/photo.ts
@@ -1,29 +1,26 @@
-
-
 export interface IPhoto{
 	id:number;
 	name:string;
 	url:string;
-	rating:number;
+	p_rating:number;
 	details:string;
 	favorite:boolean;
 }
-
 
 export class Photo implements IPhoto{
 		constructor(
 			public id:number,
 			public name:string,
 			public url:string,
-			public rating:number,
+			public p_rating:number,
 			public details:string,
 			public favorite:boolean
 		){}
 	
-	
 	setFavorite(fav:boolean):void{
-		this.favorite = !!this.favorite;
+		this.favorite = !this.favorite;
 	}
+	
 	getFavorite():boolean{
 		return this.favorite;
 	}	
